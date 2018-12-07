3946c64ae73057820f1eff7b9469fcc2a04bee26
= <treyyoung8@gmail.com>
2018-12-07 10:52:38 -0700

    added user table and review table

---
+++ server.js
@@ -0,0 +1,30 @@
+
+const express = require('express')
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const snacks = require('./routes/snacks');
+app.use('/api', snacks);
+
+app.use((req, res) => {
+  const status = 404;
+  const message = `Could not ${req.method} ${req.path}`;
+  res.status(status).json({ status, message });
+});
+
+app.use((err, _req, res, _next) => {
+  console.error(err);
+  const status = err.status || 500;
+  const message = err.message || 'Something went wrong!';
+  res.status(status).json({ message, status });
+});
+
+app.listen(port, () => {
+  console.log('listening on port', port);
+});
